314d941567d08e9a8bfea577efa8bed7cf7c04ac
21givenchy <georgekarani44@mail.com>
2022-06-17 15:43:23 +0000

    2nd

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,23 @@
 import '../styles/globals.css'
 import { MoralisProvider } from 'react-moralis'
 import { AmazonProvider } from '../context/AmazonContext'
-import { MoralisProvider } from 'react-simple-hook-modal'
+import { ModalProvider } from 'react-simple-hook-modal'
 
 function MyApp({ Component, pageProps }) {
-  return ( 
-  <MoralisProvider
-  serverUrl='[process.env.NEXT_PUBLIC_API_URL]'
-  appId='[process.env.NEXT_PUBLIC_APP_ID]'
-  >
-    <Component {...pageProps} />
-  </MoralisProvider>
-  )
+    return ( <
+        MoralisProvider serverUrl = { process.env.NEXT_PUBLIC_MORALIS_SERVER }
+        appId = { process.env.NEXT_PUBLIC_MORALIS_APP_ID } >
+        <
+        AmazonProvider >
+        <
+        ModalProvider >
+        <
+        Component {...pageProps }
+        /> <
+        /ModalProvider> <
+        /AmazonProvider> <
+        /MoralisProvider>
+    )
 }
 
 export default MyApp
