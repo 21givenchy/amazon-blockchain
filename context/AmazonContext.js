21bd7c3a76fcf759cca6d0e2bc3512f6fb5a83f8
Marvin <tallestlegacy@gmail.com>
2022-07-05 10:24:44 +0300

    fixed destroy

--- context/AmazonContext.js
+++ context/AmazonContext.js
@@ -1,23 +1,23 @@
-import { createContext, useState, useEffect } from 'react'
-import { useMoralis, useMoralisQuery } from 'react-moralis'
-import { amazonAbi, amazonCoinAddress } from '../lib/constants'
-import { ethers } from 'ethers'
+import { createContext, useState, useEffect } from "react";
+import { useMoralis, useMoralisQuery } from "react-moralis";
+import { amazonAbi, amazonCoinAddress } from "../lib/constants";
+import { ethers } from "ethers";
 
-export const AmazonContext = createContext()
+export const AmazonContext = createContext();
 
 export const AmazonProvider = ({ children }) => {
-  const [currentAccount, setCurrentAccount] = useState('')
-  const [formattedAccount, setFormattedAccount] = useState('')
-  const [balance, setBalance] = useState('')
-  const [tokenAmount, setTokenAmount] = useState('')
-  const [amountDue, setAmountDue] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
-  const [etherscanLink, setEtherscanLink] = useState('')
-  const [nickname, setNickname] = useState('')
-  const [username, setUsername] = useState('')
-  const [assets, setAssets] = useState([])
-  const [recentTransactions, setRecentTransactions] = useState([])
-  const [ownedItems, setOwnedItems] = useState([])
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [formattedAccount, setFormattedAccount] = useState("");
+  const [balance, setBalance] = useState("");
+  const [tokenAmount, setTokenAmount] = useState("");
+  const [amountDue, setAmountDue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [etherscanLink, setEtherscanLink] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [username, setUsername] = useState("");
+  const [assets, setAssets] = useState([]);
+  const [recentTransactions, setRecentTransactions] = useState([]);
+  const [ownedItems, setOwnedItems] = useState([]);
 
   const {
     authenticate,
@@ -26,46 +26,50 @@ export const AmazonProvider = ({ children }) => {
     Moralis,
     user,
     isWeb3Enabled,
-  } = useMoralis()
+  } = useMoralis();
 
   const {
     data: userData,
     error: userDataError,
     isLoading: userDataIsLoading,
-  } = useMoralisQuery('_User')
+  } = useMoralisQuery("_User");
 
   const {
     data: assetsData,
     error: assetsDataError,
     isLoading: assetsDataIsLoading,
-  } = useMoralisQuery('Assets')
+  } = useMoralisQuery("Assets");
 
-  useEffect(async () => {
-    console.log(assetsData)
-    await enableWeb3()
-    await getAssets()
-    await getOwnedAssets()
-  }, [userData, assetsData, assetsDataIsLoading, userDataIsLoading])
+  useEffect(() => {
+    (async () => {
+      console.log(assetsData);
+      await enableWeb3();
+      await getAssets();
+      await getOwnedAssets();
+    })();
+  }, [userData, assetsData, assetsDataIsLoading, userDataIsLoading]);
 
-  useEffect(async () => {
-    if (!isWeb3Enabled) {
-      await enableWeb3()
-    }
-    await listenToUpdates()
+  useEffect(() => {
+    (async () => {
+      if (!isWeb3Enabled) {
+        await enableWeb3();
+      }
+      await listenToUpdates();
 
-    if (isAuthenticated) {
-      await getBalance()
-      const currentUsername = await user?.get('nickname')
-      setUsername(currentUsername)
-      const account = await user?.get('ethAddress')
-      setCurrentAccount(account)
-      const formatAccount = account.slice(0, 5) + '...' + account.slice(-5)
-      setFormattedAccount(formatAccount)
-    } else {
-      setCurrentAccount('')
-      setFormattedAccount('')
-      setBalance('')
-    }
+      if (isAuthenticated) {
+        await getBalance();
+        const currentUsername = await user?.get("nickname");
+        setUsername(currentUsername);
+        const account = await user?.get("ethAddress");
+        setCurrentAccount(account);
+        const formatAccount = account.slice(0, 5) + "..." + account.slice(-5);
+        setFormattedAccount(formatAccount);
+      } else {
+        setCurrentAccount("");
+        setFormattedAccount("");
+        setBalance("");
+      }
+    })();
   }, [
     isWeb3Enabled,
     isAuthenticated,
@@ -76,136 +80,136 @@ export const AmazonProvider = ({ children }) => {
     setUsername,
     user,
     username,
-  ])
+  ]);
 
   const connectWallet = async () => {
-    await enableWeb3()
-    await authenticate()
-  }
+    await enableWeb3();
+    await authenticate();
+  };
 
   const buyTokens = async () => {
     if (!isAuthenticated) {
-      await connectWallet()
+      await connectWallet();
     }
 
-    const amount = ethers.BigNumber.from(tokenAmount)
-    const price = ethers.BigNumber.from('100000000000000')
-    const calcPrice = amount.mul(price)
+    const amount = ethers.BigNumber.from(tokenAmount);
+    const price = ethers.BigNumber.from("100000000000000");
+    const calcPrice = amount.mul(price);
 
-    console.log(amazonCoinAddress)
+    console.log(amazonCoinAddress);
 
     let options = {
       contractAddress: amazonCoinAddress,
-      functionName: 'mint',
+      functionName: "mint",
       abi: amazonAbi,
       msgValue: calcPrice,
       params: {
         amount,
       },
-    }
-    const transaction = await Moralis.executeFunction(options)
-    const receipt = await transaction.wait()
-    setIsLoading(false)
-    console.log(receipt)
+    };
+    const transaction = await Moralis.executeFunction(options);
+    const receipt = await transaction.wait();
+    setIsLoading(false);
+    console.log(receipt);
     setEtherscanLink(
-      `https://rinkeby.etherscan.io/tx/${receipt.transactionHash}`,
-    )
-  }
+      `https://rinkeby.etherscan.io/tx/${receipt.transactionHash}`
+    );
+  };
 
   const handleSetUsername = () => {
     if (user) {
       if (nickname) {
-        user.set('nickname', nickname)
-        user.save()
-        setNickname('')
+        user.set("nickname", nickname);
+        user.save();
+        setNickname("");
       } else {
-        console.log("Can't set empty nickname")
+        console.log("Can't set empty nickname");
       }
     } else {
-      console.log('No user')
+      console.log("No user");
     }
-  }
+  };
 
   const getBalance = async () => {
     try {
-      if (!isAuthenticated || !currentAccount) return
+      if (!isAuthenticated || !currentAccount) return;
       const options = {
         contractAddress: amazonCoinAddress,
-        functionName: 'balanceOf',
+        functionName: "balanceOf",
         abi: amazonAbi,
         params: {
           account: currentAccount,
         },
-      }
+      };
 
       if (isWeb3Enabled) {
-        const response = await Moralis.executeFunction(options)
-        console.log(response.toString())
-        setBalance(response.toString())
+        const response = await Moralis.executeFunction(options);
+        console.log(response.toString());
+        setBalance(response.toString());
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const buyAsset = async (price, asset) => {
     try {
-      if (!isAuthenticated) return
-      console.log('price: ', price)
-      console.log('asset: ', asset.name)
-      console.log(userData)
+      if (!isAuthenticated) return;
+      console.log("price: ", price);
+      console.log("asset: ", asset.name);
+      console.log(userData);
 
       const options = {
-        type: 'erc20',
+        type: "erc20",
         amount: price,
         receiver: amazonCoinAddress,
         contractAddress: amazonCoinAddress,
-      }
+      };
 
-      let transaction = await Moralis.transfer(options)
-      const receipt = await transaction.wait()
+      let transaction = await Moralis.transfer(options);
+      const receipt = await transaction.wait();
 
       if (receipt) {
         //You can do this but it's not necessary with Moralis hooks!
         // const query = new Moralis.Query('_User')
         // const results = await query.find()
 
-        const res = userData[0].add('ownedAsset', {
+        const res = userData[0].add("ownedAsset", {
           ...asset,
           purchaseDate: Date.now(),
           etherscanLink: `https://rinkeby.etherscan.io/tx/${receipt.transactionHash}`,
-        })
+        });
 
         await res.save().then(() => {
-          alert("You've successfully purchased this asset!")
-        })
+          alert("You've successfully purchased this asset!");
+        });
       }
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-  }
+  };
 
   const getAssets = async () => {
     try {
-      await enableWeb3()
+      await enableWeb3();
       // const query = new Moralis.Query('Assets')
       // const results = await query.find()
 
-      setAssets(assetsData)
+      setAssets(assetsData);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const listenToUpdates = async () => {
-    let query = new Moralis.Query('EthTransactions')
-    let subscription = await query.subscribe()
-    subscription.on('update', async object => {
-      console.log('New Transactions')
-      console.log(object)
-      setRecentTransactions([object])
-    })
-  }
+    let query = new Moralis.Query("EthTransactions");
+    let subscription = await query.subscribe();
+    subscription.on("update", async (object) => {
+      console.log("New Transactions");
+      console.log(object);
+      setRecentTransactions([object]);
+    });
+  };
 
   const getOwnedAssets = async () => {
     try {
@@ -213,15 +217,15 @@ export const AmazonProvider = ({ children }) => {
       // let results = await query.find()
 
       if (userData[0]) {
-        setOwnedItems(prevItems => [
+        setOwnedItems((prevItems) => [
           ...prevItems,
           userData[0].attributes.ownedAsset,
-        ])
+        ]);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <AmazonContext.Provider
@@ -253,5 +257,5 @@ export const AmazonProvider = ({ children }) => {
     >
       {children}
     </AmazonContext.Provider>
-  )
-}
+  );
+};
